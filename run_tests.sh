aa9e57642e9ded3791f78a5a0f63abd3a9d112b7
shridhay <47260704+shridhay@users.noreply.github.com>
2025-11-16 22:48:42 -0500

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -3,23 +3,11 @@
 # Run SAT benchmarks
 for i in {1..13}; do
     echo "Verifying SAT benchmark $i..."
-    python simplex.py "benchmarks/sat/bench$i.txt"
+    python3 simplex.py "benchmarks/sat/bench$i.txt"
 done
 
 # Run UNSAT benchmarks
 for i in {1..13}; do
     echo "Verifying UNSAT benchmark $i..."
-    python simplex.py "benchmarks/unsat/bench$i.txt"
-done
-
-# Run SAT benchmarks
-for i in {1..13}; do
-    echo "Verifying SAT benchmark $i..."
-    python simplex.py "benchmarks/sat/bench$i.txt" --i
-done
-
-# Run UNSAT benchmarks
-for i in {1..13}; do
-    echo "Verifying UNSAT benchmark $i..."
-    python simplex.py "benchmarks/unsat/bench$i.txt" --i
+    python3 simplex.py "benchmarks/unsat/bench$i.txt"
 done
